f0e818cd3b5fd55c4b923306a656bdbb59de91fd
MmYYAIS <39291075+MmYYAIS@users.noreply.github.com>
2023-05-06 15:09:26 +0800

    Update SystemRoleSettings.tsx

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -1,6 +1,7 @@
-import { Show } from 'solid-js'
-import type { Accessor, Setter } from 'solid-js'
+import { createEffect, Show } from 'solid-js'
 import IconEnv from './icons/Env'
+import IconX from './icons/X'
+import type { Accessor, Setter } from 'solid-js'
 
 interface Props {
   canEdit: Accessor<boolean>
@@ -13,27 +14,36 @@ interface Props {
 export default (props: Props) => {
   let systemInputRef: HTMLTextAreaElement
 
+  
+
   const handleButtonClick = () => {
     props.setCurrentSystemRoleSettings(systemInputRef.value)
     props.setSystemRoleEditing(false)
   }
-
+  createEffect(() => {
+    if (props.systemRoleEditing() && !props.currentSystemRoleSettings()) {
+      systemInputRef.value = '你是 ChatGPT，一个由 OpenAI 训练的大型语言模型。请仔细遵循用户的指示回答。';
+      props.setCurrentSystemRoleSettings(systemInputRef.value)
+    }
+  });
   return (
     <div class="my-4">
       <Show when={!props.systemRoleEditing()}>
         <Show when={props.currentSystemRoleSettings()}>
-          <div class="text-slate">
-            <div class="flex items-center gap-1 op-60 text-slate">
-              <IconEnv />
-              <span>System Role:</span>
+          <div>
+            <div class="fi gap-1 op-50 dark:op-60">
+              <Show when={props.canEdit()} fallback={<IconEnv />}>
+                <span onClick={() => props.setCurrentSystemRoleSettings('')} class="sys-edit-btn p-1 rd-50%" > <IconX /> </span>
+              </Show>
+              <span>System Role: </span>
             </div>
             <div class="mt-1">
-              { props.currentSystemRoleSettings() }
+              {props.currentSystemRoleSettings()}
             </div>
           </div>
         </Show>
         <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
-          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="inline-flex items-center justify-center gap-1 text-sm text-slate bg-slate/20 px-2 py-1 rounded-md transition-colors cursor-pointer hover:bg-slate/50">
+          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="sys-edit-btn">
             <IconEnv />
             <span>Add System Role</span>
           </span>
@@ -41,11 +51,11 @@ export default (props: Props) => {
       </Show>
       <Show when={props.systemRoleEditing() && props.canEdit()}>
         <div>
-          <div class="flex items-center gap-1 op-60 text-slate">
+          <div class="fi gap-1 op-50 dark:op-60">
             <IconEnv />
             <span>System Role:</span>
           </div>
-          <p class="my-2 leading-normal text-slate text-sm op-60">Gently instruct the assistant and set the behavior of the assistant.</p>
+          <p class="my-2 leading-normal text-sm op-50 dark:op-60">定义系统角色来更好的设定它的回复.</p>
           <div>
             <textarea
               ref={systemInputRef!}
@@ -53,24 +63,10 @@ export default (props: Props) => {
               autocomplete="off"
               autofocus
               rows="3"
-              w-full
-              px-3 py-3 my-1
-              min-h-12
-              max-h-36
-              text-slate
-              rounded-sm
-              bg-slate
-              bg-op-15
-              focus:bg-op-20
-              focus:ring-0
-              focus:outline-none
-              placeholder:text-slate-400
-              placeholder:op-30
-              overflow-hidden
-              resize-none scroll-pa-8px
+              gen-textarea
             />
           </div>
-          <button onClick={handleButtonClick} h-12 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 text-slate rounded-sm>
+          <button onClick={handleButtonClick} gen-slate-btn>
             Set
           </button>
         </div>
